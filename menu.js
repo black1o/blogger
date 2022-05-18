ec43bc4730bda6daea2d8cd8954dd09b7fbad5d3
black1o <69957127+black1o@users.noreply.github.com>
2022-05-18 15:07:54 +0530

    Update menu.js

--- menu.js
+++ menu.js
@@ -1,18 +1,26 @@
-(() => {
-  const hamburger = document.getElementById("hamburger");
-  const menu = document.getElementById("overlay");
-  let open = false;
-  
-  const change = () => {
-    if (!open) {
-      hamburger.classList.add("open");
-      menu.classList.add("menu");
-    } else {
-      hamburger.classList.remove("open");
-      menu.classList.remove("menu");
-    }
-    open = !open;
-  };
+var navbarMenu = document.getElementById("menu");
+var burgerMenu = document.getElementById("burger");
+var bgOverlay = document.getElementById("overlay");
 
-  hamburger.addEventListener("click", change);
-})();
+// Show Menu when Click the Burger
+// Hide Menu when Click the Overlay
+if (burgerMenu && navbarMenu && bgOverlay) {
+  burgerMenu.addEventListener("click", function () {
+    navbarMenu.classList.toggle("is-active");
+    bgOverlay.classList.toggle("is-active");
+    burgerMenu.classList.toggle("burgerMenuChange");
+  });
+
+  bgOverlay.addEventListener("click", function () {
+    navbarMenu.classList.toggle("is-active");
+    bgOverlay.classList.toggle("is-active");
+  });
+}
+
+// Hide Menu when Click the Links
+document.querySelectorAll(".menu-link").forEach(function (link) {
+  link.addEventListener("click", function () {
+    navbarMenu.classList.remove("is-active");
+    bgOverlay.classList.remove("is-active");
+  });
+});
