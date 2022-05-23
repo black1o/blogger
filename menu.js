c85b9042418a0984a44e434f0341498adf8ffe19
black1o <69957127+black1o@users.noreply.github.com>
2022-05-23 10:40:59 +0530

    Update menu.js

--- menu.js
+++ menu.js
@@ -1,26 +1,6 @@
-var navbarMenu = document.getElementById("menu");
-var burgerMenu = document.getElementById("burger");
-var bgOverlay = document.getElementById("overlay");
+const trigger = document.querySelector('.trigger');
+const nav = document.querySelector('.full-screen-nav');
+const backdrop = document.querySelector('.backdrop');
 
-// Show Menu when Click the Burger
-// Hide Menu when Click the Overlay
-if (burgerMenu && navbarMenu && bgOverlay) {
-  burgerMenu.addEventListener("click", function () {
-    navbarMenu.classList.toggle("is-active");
-    bgOverlay.classList.toggle("is-active");
-    burgerMenu.classList.toggle("burgerMenuChange");
-  });
-
-  bgOverlay.addEventListener("click", function () {
-    navbarMenu.classList.toggle("is-active");
-    bgOverlay.classList.toggle("is-active");
-  });
-}
-
-// Hide Menu when Click the Links
-document.querySelectorAll(".menu-link").forEach(function (link) {
-  link.addEventListener("click", function () {
-    navbarMenu.classList.remove("is-active");
-    bgOverlay.classList.remove("is-active");
-  });
-});
+trigger.addEventListener('click', () => nav.classList.add('open-nav'));
+backdrop.addEventListener('click', () => nav.classList.remove('open-nav'));
